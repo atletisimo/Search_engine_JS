424727a844d4b212a68eb063ecac77f7f3448345
atletisimo <37110896+atletisimo@users.noreply.github.com>
2023-03-17 11:22:50 +0100

    Update spread.js

--- spread.js
+++ spread.js
@@ -1,1 +1,17 @@
-
+const search=()=>{
+  const search_box=document.getElementById("search-item").value.toUpperCase();
+  const storeItems=document.querySelectorAll(".product-list");
+  const product=document.querySelectorAll(".product");
+  const pname=storeItems.getElementsByTagName("h2");
+  for (let i=0;i<pname.length;i++){
+    let match=product[i].getElementsByTagName("h2")[0];
+    if(match){
+    let textvalue=  match.textContent || match.innerHTML
+if(textvalue.toUpperCase().indexOf(search_box)>-1){
+  product[i].getElementsByClassName.display="";
+}else{
+  product[i].getElementsByClassName.display='none';
+}
+    }
+  }
+}
